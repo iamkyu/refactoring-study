671d9d4d167e6c6b207874a912f6270107e8e39c
kjnam <kyujnam@gmail.com>
2016-09-06 20:56:32 +0900

    init

---
+++ src/main/java/sample/Rental.java
@@ -0,0 +1,23 @@
+package sample;
+
+/**
+ * @author Kj Nam
+ * @since 2016-09-06
+ */
+public class Rental {
+    private Movie movie;
+    private int daysRented;
+
+    public Rental(Movie movie, int daysRented) {
+        this.movie = movie;
+        this.daysRented = daysRented;
+    }
+
+    public Movie getMovie() {
+        return movie;
+    }
+
+    public int getDaysRented() {
+        return daysRented;
+    }
+}
